28c8717cef356cfb0c5a6f90019a8f5f4e2d75b9
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-10-05 15:13:34 -0700

    Q2 Part 2 Complete

--- Sports-List.py
+++ Sports-List.py
@@ -1,4 +1,4 @@
-f = open("Sports-List.txt", "x")
+f = open("Sports-List.txt", "w")
 
 sports = ["basketball", "softball", "football", "baseball", "track"]  
 
@@ -7,3 +7,12 @@ f = open("Sports-List.txt", "w+")
 for i in sports:
     f.write(i+'\n') 
     f.close
+
+def get_file_lines():
+    filename = open("Sports-List.txt", "r")
+    content_list = filename.readlines()
+    for i in content_list:
+        print(i)
+    filename.close()
+    
+get_file_lines
