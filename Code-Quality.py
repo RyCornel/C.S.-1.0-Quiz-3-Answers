f6e67940d4aa724c585e28adcf119fba0bf9ea74
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-10-05 15:35:49 -0700

    Q4 Complete

--- Code-Quality.py
+++ Code-Quality.py
@@ -1,10 +1,12 @@
-# Function used to sum up numbers in a list
+# Function used to sum up integers in a list
 
-def c(a):
-    b = 0
-    for i in a:
-        b += i
-    return b
+def sum_the_list(integer_list):
+    
+    sum = 0
+    
+    for i in integer_list:
+        sum += i
+    return sum
 
 
-print(c([4, 5, 2, -3]))
+print(sum_the_list([4, 5, 2, -3]))
