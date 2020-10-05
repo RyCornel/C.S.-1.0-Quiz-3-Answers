2f8b460bed8b685e2143a208cdf53a6d748d9650
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-10-05 13:53:50 -0700

    Error correction.

--- Sports-List.py
+++
@@ -1,8 +0,0 @@
-def get_file_lines():
-    filename = open("sports.txt", "r")
-    content_list = filename.readlines()
-    for i in content_list:
-        print(i)
-    filename.close()
-    
-get_file_lines()
