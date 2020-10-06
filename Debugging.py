b348f239c22b1065f89d5c0729ce92a84f682b15
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-10-05 18:14:12 -0700

    Q1 Second corrections

--- Debugging.py
+++ Debugging.py
@@ -17,9 +17,12 @@ def get_average(num_list):
     """this functions returns the average of the numbers in the input list"""
 
     sum = 1
-    # Line 21 below needs a spelling correction, for the "num_list" remove the extra "t".
-    # Line 21 also needs a colon after the input variable "num_list".
+
+    # Line 23 below needs a spelling correction, for the "num_list" remove the extra "t".
+    # Line 23 also needs a colon after the input variable "num_list".
     for i in num_listt
+
+    # Line 26 below should read "sum += i" The "num_list[i}" variable is not needed.
         sum += num_list[i]
     return len(num_list) / sum
 
